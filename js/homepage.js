b6b661f91c47620771317b4067545c306fe8c97e
Raihan <raihanroy25@gmail.com>
2020-05-09 17:24:26 +0800

    Add files via upload

--- js/homepage.js
+++ js/homepage.js
@@ -8,46 +8,53 @@ $(function () {
     });
 });
 
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').addClass('shrink');
-    } else {
-      $('nav').removeClass('shrink');
-    }
+
+let myLabels = document.querySelectorAll('.lbl-toggle');
+
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
   });
+});
 
-// $(function(){
-//     alert("My First Jquery Test");
-//  });
-
-//  document.querySelector(".nav").addEventListener("mouseover", function(event){
-//     alert("Book a tour");
-// });
-
-
-// $(function(){
-//     $('#header_nav').data('size','big');
-//   });
-  
-//   $(window).scroll(function(){
-//     if($(document).scrollTop() > 0)
-//   {
-//       if($('#header_nav').data('size') == 'big')
-//       {
-//           $('#header_nav').data('size','small');
-//           $('#header_nav').stop().animate({
-//               height:'40px'
-//           },600);
-//       }
-//   }
-//   else
-//     {
-//       if($('#header_nav').data('size') == 'small')
-//         {
-//           $('#header_nav').data('size','big');
-//           $('#header_nav').stop().animate({
-//               height:'100px'
-//           },600);
-//         }  
+// $(window).scroll(function() {
+//     if ($(document).scrollTop() > 50) {
+//       $('nav').addClass('shrink');
+//     } else {
+//       $('nav').removeClass('shrink');
 //     }
 //   });
+
+
+//   Holder.addTheme('gray', {
+//     bg: '#777',
+//     fg: 'rgba(255,255,255,.75)',
+//     font: 'Helvetica',
+//     fontweight: 'normal'
+// //   });
+
+// var coll = document.getElementsByClassName("click-collapse");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.maxHeight){
+//       content.style.maxHeight = null;
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     } 
+//   });
+// }
+
+//   $("#viewGames").click(function(){
+//     $("#gameListPanel").removeClass('hide').slideToggle('slow');
+//   });
+  
+  
